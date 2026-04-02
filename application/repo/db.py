9ec6a3ee1b4257b00e4c41732c96ede6f4d26a29
AdamKockelbergh <adamkockelbergh@gmail.com>
2026-04-02 13:10:53 +0100

    db

---
+++ application/repo/db.py
@@ -0,0 +1,13 @@
+from application.repo.TensorRepository import TensorRepository
+
+import os
+import psycopg2
+
+def get_connection():
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
