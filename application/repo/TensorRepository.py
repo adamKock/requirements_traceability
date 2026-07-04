36b20c6508e9e9ea3a937ca752311678126df1fe
AdamKockelbergh <adamkockelbergh@gmail.com>
2026-07-04 08:45:49 +0100

    refactor db for threadpooling

--- application/repo/TensorRepository.py
+++ application/repo/TensorRepository.py
@@ -1,118 +1,122 @@
 import pickle
 import torch
 from collections import defaultdict
+from db import get_db_connection
 class TensorRepository:
 
-        def __init__(self, connection):
-                self.conn = connection
-                self.curr = connection.cursor()
-                self.curr.execute("CREATE TABLE IF NOT EXISTS test_cases (id SERIAL PRIMARY KEY, summary TEXT NOT NULL, job_id TEXT NOT NULL, embeddings BYTEA)")
-                self.curr.execute("CREATE TABLE IF NOT EXISTS test_steps (id SERIAL PRIMARY KEY, test_case_id INT REFERENCES test_cases(id), step_text TEXT NOT NULL,job_id TEXT NOT NULL, embeddings BYTEA)")
-                self.curr.execute("CREATE TABLE IF NOT EXISTS test_mappings (id SERIAL PRIMARY KEY, cannonical_field TEXT NOT NULL, varient TEXT NOT NULL)")
-                self.curr.execute("CREATE TABLE IF NOT EXISTS requirement_mappings (id SERIAL PRIMARY KEY, cannonical_field TEXT NOT NULL, varient TEXT NOT NULL)")
-                self.conn.commit()
-                self.curr.close()
+        def __init__(self):
+                pass
+                
+
+
+        def initialize_schema(self):
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("CREATE TABLE IF NOT EXISTS test_cases (id SERIAL PRIMARY KEY, summary TEXT NOT NULL, job_id TEXT NOT NULL, embeddings BYTEA)")
+                                cursor.execute("CREATE TABLE IF NOT EXISTS test_steps (id SERIAL PRIMARY KEY, test_case_id INT REFERENCES test_cases(id), step_text TEXT NOT NULL,job_id TEXT NOT NULL, embeddings BYTEA)")
+                                cursor.execute("CREATE TABLE IF NOT EXISTS test_mappings (id SERIAL PRIMARY KEY, cannonical_field TEXT NOT NULL, varient TEXT NOT NULL)")
+                                cursor.execute("CREATE TABLE IF NOT EXISTS requirement_mappings (id SERIAL PRIMARY KEY, cannonical_field TEXT NOT NULL, varient TEXT NOT NULL)")
+                                
 
         
         def create_test_case(self,summary,job_id,embedding):
-                curr = self.conn.cursor()
-                curr.execute("INSERT INTO test_cases (summary,job_id,embeddings) VALUES (%s, %s,%s) RETURNING id",(summary,job_id,pickle.dumps(embedding),))
-                test_case_id = curr.fetchone()[0]
-                self.conn.commit()
-                curr.close()  
-                return test_case_id
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("INSERT INTO test_cases (summary,job_id,embeddings) VALUES (%s, %s,%s) RETURNING id",(summary,job_id,pickle.dumps(embedding),))
+                                test_case_id = cursor.fetchone()[0]
+                                 
+                                return test_case_id
 
         
         def store_step(self, step_text, embeddings,test_case_id,job_id):
-                curr = self.conn.cursor()
-                curr.execute("INSERT INTO test_steps (test_case_id,step_text, job_id, embeddings) VALUES (%s,%s,%s,%s) RETURNING id",(test_case_id,step_text,job_id,pickle.dumps(embeddings),))
-                step_id = curr.fetchone()[0]
-                self.conn.commit()
-                curr.close()  
-                return step_id
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("INSERT INTO test_steps (test_case_id,step_text, job_id, embeddings) VALUES (%s,%s,%s,%s) RETURNING id",(test_case_id,step_text,job_id,pickle.dumps(embeddings),))
+                                step_id = cursor.fetchone()[0]
+                                 
+                                return step_id
         
         def get_test_cases_by_job_id(self, job_id):
-                cursor = self.conn.cursor()
-                cursor.execute("SELECT id, summary FROM test_cases WHERE job_id = %s", (job_id,))
-                rows = cursor.fetchall()
-                cursor.close()
-                return {row[0]: row[1] for row in rows}                   
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("SELECT id, summary FROM test_cases WHERE job_id = %s", (job_id,))
+                                rows = cursor.fetchall()
+                                
+                                return {row[0]: row[1] for row in rows}                   
 
 
 
         def get_all_test_case_embeddings(self,job_id):
-                curr = self.conn.cursor()
-                curr.execute("SELECT id, embeddings FROM test_cases WHERE job_id =%s AND embeddings IS NOT NULL ORDER BY id",(job_id,))
-                rows = curr.fetchall()
-                curr.close()
-                ids = []
-                embeddings = []
-                if not rows:
-                        return [], None
-                for test_case_id, emb_blob in rows:
-                        ids.append(test_case_id)
-                        emb = pickle.loads(emb_blob)
-                        embeddings.append(emb.cpu())
-
-                return ids, torch.stack(embeddings)
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("SELECT id, embeddings FROM test_cases WHERE job_id =%s AND embeddings IS NOT NULL ORDER BY id",(job_id,))
+                                rows = cursor.fetchall()
+                                
+                                ids = []
+                                embeddings = []
+                                if not rows:
+                                        return [], None
+                                for test_case_id, emb_blob in rows:
+                                        ids.append(test_case_id)
+                                        emb = pickle.loads(emb_blob)
+                                        embeddings.append(emb.cpu())
+                                return ids, torch.stack(embeddings)
         
         def store_test_mappings(self,Test_Case_Mapping):
-                curr = self.conn.cursor()
-                for key, variants in Test_Case_Mapping.items():
-                        for variant in variants:
-                                curr.execute("INSERT INTO test_mappings (cannonical_field, varient) VALUES (%s, %s)",(key,variant))
-                self.conn.commit()
-                curr.close()
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                for key, variants in Test_Case_Mapping.items():
+                                        for variant in variants:
+                                                cursor.execute("INSERT INTO test_mappings (cannonical_field, varient) VALUES (%s, %s)",(key,variant))
+                                                
+                                
 
         def get_test_mappings(self):
-                curr = self.conn.cursor()
-                curr.execute("SELECT cannonical_field, varient FROM test_mappings")
-                rows = curr.fetchall()
-                curr.close()
-                mapping={}
-                for standard, varient in rows:
-                        if standard not in mapping:
-                                mapping[standard]=[]
-                        mapping[standard].append(varient)
-                return mapping
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("SELECT cannonical_field, varient FROM test_mappings")
+                                rows = cursor.fetchall()
+                                
+                                mapping= defaultdict(list)
+                                for standard, varient in rows:
+                                        mapping[standard].append(varient)
+                                return dict(mapping)
 
         def store_requirement_mappings(self, requirement_mapping):
-                curr = self.conn.cursor()
-                for key, variants in requirement_mapping.items():
-                        for variant in variants:
-                                curr.execute("INSERT INTO requirement_mappings (cannonical_field, varient) VALUES (%s, %s)",(key,variant))
-                self.conn.commit()
-                curr.close()
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                for key, variants in requirement_mapping.items():
+                                        for variant in variants:
+                                                cursor.execute("INSERT INTO requirement_mappings (cannonical_field, varient) VALUES (%s, %s)",(key,variant))
+                                
 
         def get_requirement_mappings(self):
-                curr = self.conn.cursor()
-                curr.execute("SELECT cannonical_field, varient FROM requirement_mappings")
-                rows = curr.fetchall()
-                curr.close()
-                mapping ={}
-                for standard,varient in rows:
-                        if standard not in mapping:
-                                mapping[standard]=[]
-                        mapping[standard].append(varient)
-                return mapping
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                cursor.execute("SELECT cannonical_field, varient FROM requirement_mappings")
+                                rows = cursor.fetchall()
+                                
+                                mapping= defaultdict(list)
+                                for standard,varient in rows:    
+                                        mapping[standard].append(varient)
+                                return dict(mapping)
         
         def get_all_step_embeddings(self, job_id):
-                curr = self.conn.cursor()
-                # Fetch steps and link them to their parent test case
-                curr.execute("""
+                with get_db_connection() as conn:
+                        with conn.cursor() as cursor:
+                                # Fetch steps and link them to their parent test case
+                                cursor.execute("""
                         SELECT test_case_id, embeddings 
                         FROM test_steps 
                         WHERE job_id = %s AND embeddings IS NOT NULL 
                         ORDER BY test_case_id
                         """, (job_id,))
-                rows = curr.fetchall()
-                curr.close()
-
-                step_data = defaultdict(list)
-                for tc_id, emb_blob in rows:
-                        step_data[tc_id].append(pickle.loads(emb_blob).cpu())
-    
-                return step_data # Dictionary: {test_case_id: [tensor1, tensor2, ...]}
+                                rows = cursor.fetchall()
+                                
+                                step_data = defaultdict(list)
+                                for tc_id, emb_blob in rows:
+                                        step_data[tc_id].append(pickle.loads(emb_blob).cpu())
+                                return step_data # Dictionary: {test_case_id: [tensor1, tensor2, ...]}
 
                 
 
--- application/repo/db.py
+++ application/repo/db.py
@@ -1,12 +1,40 @@
 
 import os
 import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+
+
+
+DB_POOL = ThreadedConnectionPool(
+    minconn=2,
+    maxconn=10,
+    host=os.getenv("DB_HOST"),
+    dbname=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=os.getenv("DB_PORT")
+)
+
 
 def get_connection():
-    return psycopg2.connect(
+    return ThreadedConnectionPool.connect(
+         minconn=1,
+        maxconn=10,
         host=os.getenv("DB_HOST"),
         dbname=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         port=os.getenv("DB_PORT")
     )
+
+def get_db_connection():
+    """Context manager to handle automated checkout and return of connections."""
+    conn = DB_POOL.getconn()
+    try:
+        yield conn
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        DB_POOL.putconn(conn)
